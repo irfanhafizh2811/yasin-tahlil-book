e50b0ce83a4b774216ae6f306f1931261df2964b
irfanhafizh2811 <irfanhafizh2811@gmail.com>
2020-04-11 13:54:01 +0700

    Initial splash screen

---
+++ app/src/main/java/com/dzikir/tasbeeh/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dzikir.tasbeeh.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
---
+++ app/src/main/java/com/dzikir/tasbeeh/view/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.dzikir.tasbeeh.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dzikir.tasbeeh.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
